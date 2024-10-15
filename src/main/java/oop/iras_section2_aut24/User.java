e8d957e08ed5ad472b8a13613aace6535a38d757
Asif Mahmood <asif.mahmood@iub.edu.bd>
2024-10-15 12:43:06 +0600

    Added User constructor

--- src/main/java/oop/iras_section2_aut24/User.java
+++ src/main/java/oop/iras_section2_aut24/User.java
@@ -2,4 +2,9 @@ package oop.iras_section2_aut24;
 
 public class User {
     String username, password;
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
 }
