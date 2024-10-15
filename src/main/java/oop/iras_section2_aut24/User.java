0c076ee12d5ccdbea2016922a5815d13310e311c
Asif2 <asif2@iub>
2024-10-15 12:44:11 +0600

    Added getters and setters to User class

--- src/main/java/oop/iras_section2_aut24/User.java
+++ src/main/java/oop/iras_section2_aut24/User.java
@@ -1,5 +1,21 @@
 package oop.iras_section2_aut24;
 
 public class User {
-    String username, password;
+    private String username, password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
