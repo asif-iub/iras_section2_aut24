cbff2f741fd587849aca23428b863bca50b07133
Asif Mahmood <asif.mahmood@iub.edu.bd>
2024-10-15 12:13:30 +0600

    Initial commit. created a new javafx project

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module oop.iras_section2_aut24 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens oop.iras_section2_aut24 to javafx.fxml;
+    exports oop.iras_section2_aut24;
+}
---
+++ src/main/java/oop/iras_section2_aut24/HelloController.java
@@ -0,0 +1,14 @@
+package oop.iras_section2_aut24;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
