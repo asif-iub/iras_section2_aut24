bb2a01bed11cb41021930973edb6e33d828bf3df
Asif Mahmood <asif.mahmood@iub.edu.bd>
2024-10-20 11:34:03 +0600

    Added log in scene

--- src/main/java/oop/iras_section2_aut24/HelloController.java
+++ src/main/java/oop/iras_section2_aut24/HelloController.java
@@ -2,13 +2,29 @@ package oop.iras_section2_aut24;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    private Label msgLabel;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("User class should be present!");
+    private TextField usernameInput;
+
+    @FXML
+    private PasswordField passwordInput;
+
+    @FXML
+    protected void onSignInButtonClick() {
+        String username = usernameInput.getText();
+        String password = passwordInput.getText();
+
+        if (username.equals("asif") && password.equals("1234")) {
+            msgLabel.setText("Log in successful!");
+        }
+        else{
+            msgLabel.setText(("Log in failed!"));
+        }
     }
 }
