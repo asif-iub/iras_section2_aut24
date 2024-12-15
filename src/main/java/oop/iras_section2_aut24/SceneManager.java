8c76f2a8746dab9a6a3235ee65774cd31d93fbd5
Asif Mahmood <asif.mahmood@iub.edu.bd>
2024-12-15 09:10:02 +0600

    Implemented Issue-001: Show confirmation dialog during log out

--- src/main/java/oop/iras_section2_aut24/SceneManager.java
+++ src/main/java/oop/iras_section2_aut24/SceneManager.java
@@ -3,9 +3,12 @@ package oop.iras_section2_aut24;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Optional;
 
 public class SceneManager {
     private static Stage stage;
@@ -38,6 +41,13 @@ public class SceneManager {
     }
 
     public static void logOut() throws IOException {
-        switchScene("hello-view.fxml");
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION, "Confirm log out?");
+        alert.setTitle("Log Out");
+        alert.setHeaderText("Confirm log out");
+        alert.setContentText("Are you sure you want to log out?");
+        Optional<ButtonType> result = alert.showAndWait();
+        if (result.isPresent() && result.get() == ButtonType.OK) {
+            switchScene("hello-view.fxml");
+        }
     }
 }
